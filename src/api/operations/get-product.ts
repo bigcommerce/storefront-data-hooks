c6b655cba5f8ea1508bc7aa3023c6d23c73f3ef5
Luis Alvarez <luis@vercel.com>
2020-10-27 05:13:52 -0500

    Updated hooks and operations

--- src/api/operations/get-product.ts
+++ src/api/operations/get-product.ts
@@ -1,5 +1,4 @@
 import type { GetProductQuery, GetProductQueryVariables } from '../../schema'
-import type { RecursivePartial, RecursiveRequired } from '../utils/types'
 import setProductLocaleMeta from '../utils/set-product-locale-meta'
 import { productInfoFragment } from '../fragments/product'
 import { BigcommerceConfig, getConfig } from '..'
@@ -16,6 +15,38 @@ export const getProductQuery = /* GraphQL */ `
           __typename
           ... on Product {
             ...productInfo
+            variants {
+              edges {
+                node {
+                  entityId
+                  defaultImage {
+                    urlOriginal
+                    altText
+                    isDefault
+                  }
+                  prices {
+                    ...productPrices
+                  }
+                  inventory {
+                    aggregated {
+                      availableToSell
+                      warningLevel
+                    }
+                    isInStock
+                  }
+                  productOptions {
+                    edges {
+                      node {
+                        __typename
+                        entityId
+                        displayName
+                        ...multipleChoiceOption
+                      }
+                    }
+                  }
+                }
+              }
+            }
           }
         }
       }
@@ -42,12 +73,14 @@ export type ProductVariables = { locale?: string } & (
 async function getProduct(opts: {
   variables: ProductVariables
   config?: BigcommerceConfig
+  preview?: boolean
 }): Promise<GetProductResult>
 
 async function getProduct<T extends { product?: any }, V = any>(opts: {
   query: string
   variables: V
   config?: BigcommerceConfig
+  preview?: boolean
 }): Promise<GetProductResult<T>>
 
 async function getProduct({
@@ -58,6 +91,7 @@ async function getProduct({
   query?: string
   variables: ProductVariables
   config?: BigcommerceConfig
+  preview?: boolean
 }): Promise<GetProductResult> {
   config = getConfig(config)
 
@@ -68,20 +102,14 @@ async function getProduct({
     hasLocale: !!locale,
     path: slug ? `/${slug}/` : vars.path!,
   }
-  const { data } = await config.fetch<RecursivePartial<GetProductQuery>>(
-    query,
-    { variables }
-  )
+  const { data } = await config.fetch<GetProductQuery>(query, { variables })
   const product = data.site?.route?.node
 
   if (product?.__typename === 'Product') {
     if (locale && config.applyLocale) {
       setProductLocaleMeta(product)
     }
-
-    return {
-      product: product as RecursiveRequired<typeof product>,
-    }
+    return { product }
   }
 
   return {}
