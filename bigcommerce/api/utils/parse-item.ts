262164c80953060d59cf58129962b507cfb2a63c
luc <luc.leray@gmail.com>
2020-10-27 02:11:16 +0100

    move lib/bigcommerce and lib/commerce

--- bigcommerce/api/utils/parse-item.ts
+++
@@ -1,9 +0,0 @@
-import type { ItemBody } from '../cart'
-
-const parseItem = (item: ItemBody) => ({
-  quantity: item.quantity,
-  product_id: item.productId,
-  variant_id: item.variantId,
-})
-
-export default parseItem
