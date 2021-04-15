64f6481a80e999a05bfeb7632c3b1830b09d49f6
jorgemasta <jorgems93@gmail.com>
2021-04-15 16:01:42 +0100

    style: remove spaces

--- src/api/address/types.ts
+++ src/api/address/types.ts
@@ -7,84 +7,84 @@ export type FormFieldValue =
   | CustomerFormFieldValue
   | CustomerAddressFormFieldValue
 
-	export interface Address {
-		/**
-		 * The first name of the customer address.
-		 */
-		first_name: string
-		/**
-		 * The last name of the customer address.
-		 */
-		last_name: string
-		/**
-		 * The company of the customer address.
-		 */
-		company?: string
-		/**
-		 * The address 1 line.
-		 */
-		address1: string
-		/**
-		 * The address 2 line.
-		 */
-		address2?: string
-		/**
-		 * The city of the customer address.
-		 */
-		city: string
-		/**
-		 * The state or province name
-		 */
-		state_or_province: string
-		/**
-		 * The postal code of the customer address.
-		 */
-		postal_code: string
-		/**
-		 * The country code of the customer address.
-		 */
-		country_code: string
-		/**
-		 * The phone number of the customer address.
-		 */
-		phone?: string
-		address_type?: AddressType
-		/**
-		 * The customer ID.
-		 */
-		customer_id: number
-		/**
-		 * The unique numeric ID of the address.
-		 */
-		id: number
-		/**
-		 * The country name of the customer address.
-		 */
-		country?: string
-		/**
-		 * Array of form fields. Controlled by `formfields` parameter.
-		 */
-		form_fields?: FormFieldValue[]
-	}
-	export interface CustomerFormFieldValue {
-		/**
-		 * The form field name.
-		 */
-		name: string
-		value: string | number | string[]
-		customer_id: number
-	}
-	export interface CustomerAddressFormFieldValue {
-		/**
-		 * The form field name.
-		 */
-		name: string
-		value: string | number | string[]
-		/**
-		 * The Customer Address ID.
-		 */
-		address_id: number
-	}
+export interface Address {
+	/**
+	 * The first name of the customer address.
+	 */
+	first_name: string
+	/**
+	 * The last name of the customer address.
+	 */
+	last_name: string
+	/**
+	 * The company of the customer address.
+	 */
+	company?: string
+	/**
+	 * The address 1 line.
+	 */
+	address1: string
+	/**
+	 * The address 2 line.
+	 */
+	address2?: string
+	/**
+	 * The city of the customer address.
+	 */
+	city: string
+	/**
+	 * The state or province name
+	 */
+	state_or_province: string
+	/**
+	 * The postal code of the customer address.
+	 */
+	postal_code: string
+	/**
+	 * The country code of the customer address.
+	 */
+	country_code: string
+	/**
+	 * The phone number of the customer address.
+	 */
+	phone?: string
+	address_type?: AddressType
+	/**
+	 * The customer ID.
+	 */
+	customer_id: number
+	/**
+	 * The unique numeric ID of the address.
+	 */
+	id: number
+	/**
+	 * The country name of the customer address.
+	 */
+	country?: string
+	/**
+	 * Array of form fields. Controlled by `formfields` parameter.
+	 */
+	form_fields?: FormFieldValue[]
+}
+export interface CustomerFormFieldValue {
+	/**
+	 * The form field name.
+	 */
+	name: string
+	value: string | number | string[]
+	customer_id: number
+}
+export interface CustomerAddressFormFieldValue {
+	/**
+	 * The form field name.
+	 */
+	name: string
+	value: string | number | string[]
+	/**
+	 * The Customer Address ID.
+	 */
+	address_id: number
+}
 
 /**
  * Pagination links for the previous and next parts of the whole collection.
