8440962bcf3f90ad6f651293a8db50b3bdc61e24
jorgemasta <jorgems93@gmail.com>
2021-04-22 17:29:33 +0100

    fix: set prototypes to our errors

--- src/api/utils/errors.ts
+++ src/api/utils/errors.ts
@@ -1,7 +1,13 @@
 import type { Response } from '@vercel/fetch'
 
 // Used for GraphQL errors
-export class BigcommerceGraphQLError extends Error {}
+export class BigcommerceGraphQLError extends Error {
+  constructor(msg: string) {
+    super(msg)
+    // Necessary to compile to ES5: https://github.com/microsoft/TypeScript/issues/22585
+    Object.setPrototypeOf(this, BigcommerceGraphQLError.prototype);
+  }
+}
 
 export class BigcommerceApiError extends Error {
   status: number
@@ -10,6 +16,8 @@ export class BigcommerceApiError extends Error {
 
   constructor(msg: string, res: Response, data?: any) {
     super(msg)
+    // Necessary to compile to ES5: https://github.com/microsoft/TypeScript/issues/22585
+    Object.setPrototypeOf(this, BigcommerceApiError.prototype);
     this.name = 'BigcommerceApiError'
     this.status = res.status
     this.res = res
@@ -20,6 +28,8 @@ export class BigcommerceApiError extends Error {
 export class BigcommerceNetworkError extends Error {
   constructor(msg: string) {
     super(msg)
+    // Necessary to compile to ES5: https://github.com/microsoft/TypeScript/issues/22585
+    Object.setPrototypeOf(this, BigcommerceNetworkError.prototype);
     this.name = 'BigcommerceNetworkError'
   }
 }
