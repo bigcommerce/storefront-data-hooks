df77b38f27ec1c10cf74c1b0f5f04c97679dd4f4
Vinny Listrani <vinny@tri.be>
2022-03-30 16:14:35 -0500

    feat: BCP20-1205 add grapohQL network error mesage.

--- src/api/utils/fetch-graphql-api.ts
+++ src/api/utils/fetch-graphql-api.ts
@@ -1,28 +1,38 @@
+import { Response } from '@vercel/fetch'
 import { FetcherError } from '../.././commerce/utils/errors'
 import type { GraphQLFetcher } from '../.././commerce/api'
 import { getConfig } from '..'
 import fetch from './fetch'
+import { BigcommerceNetworkError } from './errors'
 
 const fetchGraphqlApi: GraphQLFetcher = async (
   query: string,
   { variables, preview } = {},
   fetchOptions
 ) => {
-  // log.warn(query)
   const config = getConfig()
-  const res = await fetch(config.commerceUrl + (preview ? '/preview' : ''), {
-    ...fetchOptions,
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${config.apiToken}`,
-      ...fetchOptions?.headers,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  })
+  let res: any = Response
+  try {
+    res = await fetch(config.commerceUrl + (preview ? '/preview' : ''), {
+      ...fetchOptions,
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${config.apiToken}`,
+        ...fetchOptions?.headers,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+  } catch (error) {
+    throw new BigcommerceNetworkError(
+      `Fetch to Bigcommerce GraphQL [${
+        config.commerceUrl + (preview ? '/preview' : '')
+      }] failed: ${error.message}`
+    )
+  }
 
   const json = await res.json()
   if (json.errors) {
